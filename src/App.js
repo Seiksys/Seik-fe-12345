c8eda97dace6232c356e81355f22e23acf78b677
Seiksys <seikyusengsys@gmail.com>
2024-05-17 01:17:38 +0800

    new
    new

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
+import DataFetcher from "./DataFetcher"; // 导入 DataFetcher 组件
 import "./components/Card.css";
-import UpdateUserButton from './UpdateUserButton';
 
 function MainApp() {
   return (
     <div className="bg-dark">
       <Header />
       <Card />
+      <DataFetcher /> {/* 使用 DataFetcher 组件 */}
       <Footer />
     </div>
   );
